a8c5ecae714f956b1b9541d36dd946418f3dbf43
dwfy <dwfy_yx@163.com>
2019-09-19 14:03:14 +0800

    extract github config from code to properties

---
+++ src/main/java/com/dwfy/community/controller/AuthorizeController.java
@@ -0,0 +1,39 @@
+package com.dwfy.community.controller;
+
+import com.dwfy.community.dto.AccessTokenDTO;
+import com.dwfy.community.dto.GithubUser;
+import com.dwfy.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Value("${github.client.id}")
+    private String clientID;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.Redirect.uri}")
+    private String RedirectUri;
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientID);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(RedirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        return "index";
+    }
+
+}
