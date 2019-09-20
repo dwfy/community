cb059cc3da068e4da708e09721d8c3c20ff8332d
dwfy <dwfy_yx@163.com>
2019-09-20 11:18:46 +0800

    h2

--- src/main/java/com/dwfy/community/controller/AuthorizeController.java
+++ src/main/java/com/dwfy/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.dwfy.community.controller;
 
 import com.dwfy.community.dto.AccessTokenDTO;
 import com.dwfy.community.dto.GithubUser;
+import com.dwfy.community.mapper.UserMapper;
+import com.dwfy.community.model.User;
 import com.dwfy.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,6 +27,9 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -35,10 +41,17 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(RedirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null){
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setAccount_id(String.valueOf(githubUser.getId()));
+            user.setName(githubUser.getName());
+            user.setGmt_create(System.currentTimeMillis());
+            user.setGmt_modified(user.getGmt_create());
+            userMapper.insert(user);
             //登录成功，写cookies和session
-            request.getSession().setAttribute("user",user);
+            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
 
         }else {
